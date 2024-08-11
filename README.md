0bfa1da06dae5c7e107a7e7ffe4dd5824cde7ad6
David Mertz <mertz@gnosis.cx>
2024-08-11 12:10:13 -0400

    Simplify and clarify recur() code

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Simple Python module for run-length encoding iteratables.
 
 ```python
 from rle import *
-orig = [1,1,1,1,2,3,4,4,3,3,3]
+orig = [1, 1, 1, 1, 2, 3, 4, 4, 3, 3, 3]
 rle = [(1, 4), (2, 1), (3, 1), (4, 2), (3, 3)]
 assert list(rle_encode(orig)) == rle
 assert list(rle_decode(rle)) == orig
@@ -15,8 +15,8 @@ assert list(rle_decode(rle)) == orig
 Just for fun, throw in an infinite sequence.
 
 ```python
-from rle import make_fib
-assert list(islice(make_fib(), 8)) == [1, 1, 2, 3, 5, 8, 13, 21]
+from fib import recur
+assert list(islice(recur(), 8)) == [1, 1, 2, 3, 5, 8, 13, 21]
 ```
 
 No Copyright, Public Domain
--- fib.py
+++ fib.py
@@ -1,18 +1,37 @@
-from itertools import accumulate, islice, repeat, tee
-
-__fib = (
-    lambda init: map(
-        lambda x: x[0], accumulate(repeat(init), lambda s, _: (s[1], sum(s)))
-    )
-)((1, 1))
+from itertools import accumulate, islice, repeat
 
 
-def make_fib():
-    global __fib
-    __fib, fib = tee(__fib)
-    return fib
+def first(it):
+    return next(iter(it))
+
+
+def recur(seed=(1, 1)):
+    "Generate Fibonacci numbers or generalizations thereof"
+    return map(first, accumulate(repeat(seed), lambda s, _: (s[1], sum(s))))
 
 
 if __name__ == "__main__":
-    print(list(islice(make_fib(), 8)))
-    print(list(islice(make_fib(), 15)))
+    fib1, fib2 = recur(), recur()
+    assert list(islice(fib1, 8)) == [1, 1, 2, 3, 5, 8, 13, 21]
+    assert list(islice(fib1, 8)) == [34, 55, 89, 144, 233, 377, 610, 987]
+    assert list(islice(fib2, 15)) == [
+        1,
+        1,
+        2,
+        3,
+        5,
+        8,
+        13,
+        21,
+        34,
+        55,
+        89,
+        144,
+        233,
+        377,
+        610,
+    ]
+    # Successive Lucas numbers, like Fibonacci, have a ratio that
+    # converges to the golden ratio, phi (φ ≈ 1.618).
+    lucas = recur((2, 1))
+    assert list(islice(lucas, 8)) == [2, 1, 3, 4, 7, 11, 18, 29]
